b7f81914cf9feff7ae3d4ee7063f009bb162c2d5
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2022-02-18 14:21:32 +0100

    small script used to get block data

---
+++ explorer.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+uri = URI.parse('http://localhost:8545/verkle')
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri.request_uri, 
+          'Content-Type' => 'application/json')
+request.body = '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}'
+resp = http.request(request)
+
+raise("request failed #{resp.code.class}") if resp.code.to_i != 200
+
+data = JSON.parse(resp.body)
+num = data['result'].hex
+puts "latest block number = #{num}"
+
+num.times do |bnum|
+  next if bnum == 0
+  request.body = '{"method":"eth_getBlockNumber","params":["' + format("%#x", bnum) + '"],"id":1,"jsonrpc":"2.0"}'
+  resp = http.request(request)
+end
